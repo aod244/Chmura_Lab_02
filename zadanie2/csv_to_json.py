b4e17f14ef80ae300c1d928a01b237489563576d
Filip Ostrowski <filip.ostrowski96@gmail.com>
2022-12-20 14:36:51 +0100

    1

---
+++ zadanie2/csv_to_json.py
@@ -0,0 +1,37 @@
+import csv, json, os
+import pandas as pd
+import matplotlib.colors as colors
+
+master_df = pd.DataFrame()
+
+for file in os.listdir(os.getcwd()):
+    if file.endswith('.csv'):
+        master_df = master_df.append(pd.read_csv(file))
+
+master_df.to_csv('combined.csv', index=False)
+
+df = pd.read_csv('combined.csv')
+hex_value = df['value'].tolist()
+rgb = []
+for i in hex_value:
+    new_i = colors.hex2color(i)
+    rgb.append(new_i)
+
+df['rgb'] = rgb 
+
+df.to_csv('combined.csv') 
+
+with open ("combined.csv", "r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    data = {"color": []}
+    for row in reader:
+        data["color"].append({"color name": row[1], "Hex": row[2], "RGB": row[3]})
+
+with open ("colors_combined.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+file = 'combined.csv' 
+
+if(os.path.exists(file) and os.path.isfile(file)):
+    os.remove(file)
--- zadanie2/script.py
+++ zadanie2/script.py
@@ -1,33 +1,50 @@
 import csv, json, os
 import pandas as pd
 import matplotlib.colors as colors
+import boto3
 
-master_df = pd.DataFrame()
+def lambda_handler(event, context):
 
-for file in os.listdir(os.getcwd()):
-    if file.endswith('.csv'):
-        master_df = master_df.append(pd.read_csv(file))
+    master_df = pd.DataFrame()
 
-master_df.to_csv('combined.csv', index=False)
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.csv'):
+            master_df = master_df.append(pd.read_csv(file))
 
-df = pd.read_csv('combined.csv')
-hex_value = df['value'].tolist()
-rgb = []
-for i in hex_value:
-    new_i = colors.hex2color(i)
-    rgb.append(new_i)
+    master_df.to_csv('combined.csv', index=False)
 
-df['rgb'] = rgb 
+    df = pd.read_csv('combined.csv')
+    hex_value = df['value'].tolist()
+    rgb = []
+    for i in hex_value:
+        new_i = colors.hex2color(i)
+        rgb.append(new_i)
 
-df.to_csv('combined.csv') 
+    df['rgb'] = rgb 
 
-with open ("combined.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)
-    data = {"color": []}
-    for row in reader:
-        data["color"].append({"color name": row[1], "Hex": row[2], "RGB": row[3]})
+    df.to_csv('combined.csv') 
 
-with open ("names.json", "w") as file:
-    json.dump(data, file, indent=4)
- 
+    targetbucket = 'Wiaderko'
+    csvkey = 'combined.csv'
+    jsonkey = 'color.json'
+
+    s3 = boto3.resource('s3')
+    csv_object = s3.Object(targetbucket, csvkey)
+    csv_content = csv_object.get()['Body'].read().splitlines()
+    s3_client = boto3.client('s3')
+    l=[]
+
+    for line in csv_content:
+        x = json.dumps(line.decode('utf-8')).split(',')
+        colorname = str(x[1])
+        hex = str(x[2])
+        rgb = str(x[3])
+        y = '{ "Color name": ' + colorname + '"' + ',' + ' "Hex":' + '"' + hex + '"' + ',' + ' "RGB":' + '"' + rgb + '"' + '}'
+        l.append(y)
+
+    s3_client.put_object(
+        Bucket=targetbucket,
+        Body = str(l).replace("'",""),
+        Key = jsonkey,
+        ServerSideEncryption= 'AES256'
+    )
